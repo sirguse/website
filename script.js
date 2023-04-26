4345cff171490419d92b755ffcb49368919baf23
sirguse <tesfov@gmail.com>
2023-04-26 16:56:44 +0200

    2 commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+AOS.init();
 
 
 
@@ -8,6 +8,79 @@ toggleClick.addEventListener("click", ()=> {
     toggleClick.classList.toggle('active');
     Kosz.classList.toggle("active");
 })
-document.getElementById('About').onclick = function() {
-    window.scrollTo(0,document.body.scrollHeight);
+
+// Do poprawy
+/*
+const obrazek = document.getElementById('Profil');
+
+let initialX;
+let initialY;
+
+obrazek.addEventListener('mouseenter', (event) => {
+  initialX = event.clientX;
+  initialY = event.clientY;
+});
+
+obrazek.addEventListener('mousemove', (event) => {
+  const diffX = event.clientX - initialX;
+  const diffY = event.clientY - initialY;
+
+  obrazek.style.transform = `translate(${diffX * 0.1}px, ${diffY * 0.1}px)`;
+});
+
+obrazek.addEventListener('mouseleave', () => {
+  obrazek.style.transform = `translate(0px, 0px)`;
+});
+
+obrazek.addEventListener('mousemove', (event) => {
+  const diffX = event.clientX - initialX;
+  const diffY = event.clientY - initialY;
+
+  obrazek.style.transform = `translate(${diffX * 0.3}px, ${diffY * 0.3}px)`;
+});
+*/
+const obrazek = document.getElementById('Profil');
+
+let initialX;
+let initialY;
+
+obrazek.addEventListener('mouseenter', (event) => {
+  initialX = event.clientX;
+  initialY = event.clientY;
+});
+
+obrazek.addEventListener('mousemove', (event) => {
+  const diffX = event.clientX - initialX;
+  const diffY = event.clientY - initialY;
+
+  obrazek.style.transition = 'all 0.3s';
+  obrazek.style.transform = `translate(${diffX * 0.15}px, ${diffY * 0.15}px)`;
+});
+
+obrazek.addEventListener('mouseleave', () => {
+  obrazek.style.transform = `translate(0px, 0px)`;
+});
+
+
+
+
+
+
+
+window.addEventListener("scroll", reveal);
+
+function reveal(){
+  var reveals = document.querySelectorAll(".reveal");
+  for(var i = 0; i < reveals.length; i++){
+    var windowheight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint = 150;
+
+    if(revealtop < windowheight - revealpoint){
+      reveals[i].classList.add("active");
+    }
+    else{
+      reveals[i].classList.remove("active");
+    }
   }
+}
