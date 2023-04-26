8d4bfe9e4dbf0afd33a4182679539bb3d8887740
sirguse <tesfov@gmail.com>
2023-04-26 16:55:44 +0200

    Poprawione

--- script.js
+++ script.js
@@ -8,3 +8,6 @@ toggleClick.addEventListener("click", ()=> {
     toggleClick.classList.toggle('active');
     Kosz.classList.toggle("active");
 })
+document.getElementById('About').onclick = function() {
+    window.scrollTo(0,document.body.scrollHeight);
+  }
